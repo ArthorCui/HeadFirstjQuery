ecb1d725aa294c687868da715d79f482231ab2d6
arthor_cui <arthor.cui@gmail.com>
2014-08-11 20:24:30 +0800

    add single function to handle guess_box show and format js code(ctrl+alt+f)

--- ch03/scripts/my_script.js
+++ ch03/scripts/my_script.js
@@ -1,11 +1,13 @@
-$(document).ready(function(){
-	$(".guess_box").click(function(){
-		var discount = Math.floor((Math.random()*5) + 5);
+$(document).ready(function() {
+	$(".guess_box").click(checkForCode);
+
+	function checkForCode() {
+		var discount = Math.floor((Math.random() * 5) + 5);
 		var discount_msg = "<p>Your discount is " + discount + "%</p>";
 		$(this).append(discount_msg);
 
-		$(".guess_box").each(function(){
+		$(".guess_box").each(function() {
 			$(this).unbind('click');
 		});
-	});
+	}
 });
