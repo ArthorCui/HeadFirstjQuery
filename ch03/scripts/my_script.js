b2b644937969c62b6e438255d0d2c5ac632dbf6d
arthor_cui <arthor.cui@gmail.com>
2014-08-11 20:41:40 +0800

    add get random function hide code function handle whether discount when click

--- ch03/scripts/my_script.js
+++ ch03/scripts/my_script.js
@@ -1,11 +1,36 @@
 $(document).ready(function() {
 	$(".guess_box").click(checkForCode);
 
-	function checkForCode() {
-		var discount = Math.floor((Math.random() * 5) + 5);
-		var discount_msg = "<p>Your discount is " + discount + "%</p>";
-		$(this).append(discount_msg);
+	function getRandom(num) {
+		var my_num = Math.floor((Math.random() * 5) + 5);
+		return my_num;
+	}
 
+	var hideCode = function() {
+
+		var numRand = getRandom(4);
+		$(".guess_box").each(function(index, value) {
+			if (numRand == index) {
+				$(this).append("<span id='has_discount'></span>");
+				return false;
+			}
+		});
+	}
+
+	hideCode();
+
+	function checkForCode() {
+
+		var discount;
+		if ($.contains(this, document.getElementById("has_discount"))) {
+
+			var my_num = getRandom(5);
+			discount = "<p>Your Discount is " + my_num + "%</p>";
+		} else {
+			discount = "<p>Sorry, no discount this time!</p>";
+		}
+
+		$(this).append(discount);
 		$(".guess_box").each(function() {
 			$(this).unbind('click');
 		});
