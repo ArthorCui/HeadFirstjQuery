0a9737066933f4f181823730d6e0a28aa4297cb9
arthor_cui <arthor.cui@gmail.com>
2014-08-11 20:21:23 +0800

    another method to realize click bind and unbind show single discount

--- ch03/scripts/my_script.js
+++ ch03/scripts/my_script.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
 		var discount = Math.floor((Math.random()*5) + 5);
 		var discount_msg = "<p>Your discount is " + discount + "%</p>";
 		$(this).append(discount_msg);
-		$(".guess_box").unbind('click');
+
+		$(".guess_box").each(function(){
+			$(this).unbind('click');
+		});
 	});
 });
