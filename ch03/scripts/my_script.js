bd38facf834017920d3666d84040eea9b3af7d9f
arthor_cui <arthor.cui@gmail.com>
2014-08-11 20:18:14 +0800

    add ch03 file exercise on the ch02 modified basic

---
+++ ch03/scripts/my_script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".guess_box").click(function(){
+		var discount = Math.floor((Math.random()*5) + 5);
+		var discount_msg = "<p>Your discount is " + discount + "%</p>";
+		$(this).append(discount_msg);
+		$(".guess_box").unbind('click');
+	});
+});
