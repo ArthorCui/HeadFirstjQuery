6298f775f1224c4b12a75a9a72677e12b68685e1
arthor_cui <arthor.cui@gmail.com>
2014-08-12 13:39:16 +0800

    add script for element add and store and remove and show from store

--- ch04/scripts/my_scripts.js
+++ ch04/scripts/my_scripts.js
@@ -1,5 +1,36 @@
 $(document).ready(function() {
 
-	$f = $(".fish").parent().parent().detach();
-	$(".hamburger").replaceWith("<li class='portobello'><em>portobello Mushroom</em></li>");
+	var click_status = status;
+
+	$("button#vegOn").click(function() {
+
+		if (click_status == false) {
+
+			$f = $(".fish").parent().parent().detach();
+			$(".hamburger").replaceWith("<li class='portobello'><em>portobello Mushroom</em></li>");
+			$(".meat").after("<li class='tofu'><em>Tofu</em></li>");
+			$m = $(".meat").detach();
+			$(".portobello").parent().parent().addClass("veg_leaf");
+			$(".tofu").parent().parent().addClass("veg_leaf");
+
+			click_status = true;
+		}
+	});
+
+	$("button#restoreMe").click(function() {
+		if (click_status == true) {
+
+			$(".portobello").replaceWith("<li class='hamburger'>Hamburger</li>");
+			$(".menu_entrees li").first().before($f);
+			$(".tofu").each(function(i) {
+				$(this).after($m[i]);
+
+			});
+			$(".hamburger").parent().parent().removeClass("veg_leaf");
+			$(".tofu").parent().parent().removeClass("veg_leaf");
+			$(".tofu").remove();
+
+			click_status = false;
+		}
+	});
 });
