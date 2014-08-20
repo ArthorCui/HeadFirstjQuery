065b3d2ea01d27e19b7ce5e86ae63de0c11b6c35
arthor_cui <arthor.cui@gmail.com>
2014-08-20 15:05:12 +0800

    add customization function to repeat code and fix window keywords

--- ch07/scripts/my_scripts.js
+++ ch07/scripts/my_scripts.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
 
-	var headclix = 0,
-		eyesclix = 0,
-		noseclix = 0,
-		mouthclix = 0;
-
 	goLightning();
-	windows.onblur = stopLightning();
-	windows.onfocus = goLightning();
+	window.onblur = stopLightning();
+	window.onfocus = goLightning();
 
 	var int1, int2, int3;
 
@@ -28,77 +23,55 @@ $(document).ready(function() {
 
 	function stopLightning() {
 
-		windows.clearInterval(int1);
-		windows.clearInterval(int2);
-		windows.clearInterval(int3);
+		window.clearInterval(int1);
+		window.clearInterval(int2);
+		window.clearInterval(int3);
 	}
 
+	var clix = [0, 0, 0, 0];
 
 	$("#head").click(function() {
-		if (headclix < 9) {
-			$(this).animate({
-				left: "-=367px"
-			}, 500);
-			headclix += 1;
-		} else {
-			$(this).animate({
-				left: "0px"
-			}, 500);
-			headclix = 0;
-		}
+		moveMe(0, this);
 	});
 
 	$("#eyes").click(function() {
-		if (eyesclix < 9) {
-			$(this).animate({
-				left: "-=367px"
-			}, 500);
-			eyesclix += 1;
-		} else {
-			$(this).animate({
-				left: "0px"
-			}, 500);
-			eyesclix = 0;
-		}
+		moveMe(1, this);
 	});
 
 	$("#nose").click(function() {
-		if (noseclix < 9) {
-			$(this).animate({
-				left: "-=367px"
-			}, 500);
-			noseclix += 1;
-		} else {
-			$(this).animate({
-				left: "0px"
-			}, 500);
-			noseclix = 0;
-		}
+		moveMe(2, this);
 	});
 
 	$("#mouth").click(function() {
-		if (mouthclix < 9) {
-			$(this).animate({
+		moveMe(3, this);
+	});
+
+
+	function moveMe(i, obj) {
+
+		if (clix[i] < 9) {
+			$(obj).animate({
 				left: "-=367px"
 			}, 500);
-			mouthclix += 1;
+			clix[i] += 1;
 		} else {
-			$(this).animate({
+			$(obj).animate({
 				left: "0px"
 			}, 500);
-			mouthclix = 0;
+			clix[i] = 0;
 		}
-	});
+	}
+
+	function lightning_one() {
+		$("#container #lightning1").fadeIn(250).fadeOut(250);
+	};
+
+	function lightning_two() {
+		$("#container #lightning2").fadeIn("fast").fadeOut("slow");
+	};
+
+	function lightning_three() {
+		$("#container #lightning3").fadeIn("slow").fadeOut("fast");
+	};
+
 });
-
-function lightning_one() {
-	$("#container #lightning1").fadeIn(250).fadeOut(250);
-};
-
-function lightning_two() {
-	$("#container #lightning2").fadeIn("fast").fadeOut("slow");
-};
-
-function lightning_three() {
-	$("#container #lightning3").fadeIn("slow").fadeOut("fast");
-};
